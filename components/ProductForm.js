36d8a7c76c56d39dfbb30f29237a4e2f7a3ed586
Artem Plohenko <58325944+ArtemPlohenko@users.noreply.github.com>
2023-11-10 22:19:04 +0200

    added block with Photos

--- components/ProductForm.js
+++ components/ProductForm.js
@@ -7,10 +7,12 @@ export default function ProductForm({
   title: existingTitle,
   description: existingDescription,
   price: existingPrice,
+  images: existingImages,
 }) {
   const [title, setTitle] = useState(existingTitle || "");
   const [description, setDescription] = useState(existingDescription || "");
   const [price, setPrice] = useState(existingPrice || "");
+  const [images, setImages] = useState(existingImages || []);
   const [goToProducts, setGoToProducts] = useState(false);
   const router = useRouter();
 
@@ -18,7 +20,7 @@ export default function ProductForm({
 
   async function saveProduct(ev) {
     ev.preventDefault();
-    const data = { title, description, price };
+    const data = { title, description, price, images };
 
     if (_id) {
       //update
@@ -34,10 +36,65 @@ export default function ProductForm({
     router.push("/products");
   }
 
+  async function uploadImage(ev) {
+    // ev.preventDefault();
+
+    // console.log(ev);
+    const files = ev.target?.files;
+    if (files?.length > 0) {
+      const data = new FormData();
+
+      for (const file of files) {
+        data.append("file", file);
+      }
+
+      // files.forEach((file) => data.append("file", file));
+
+      // const res = await axios.post("/api/upload", data, {
+      //   headers: { "Content-Type": "multipart/form-data" },
+      // });
+
+      const res = await axios.post("/api/upload", data);
+
+      // console.log(res.data);
+      setImages((oldImages) => {
+        return [...oldImages, ...res.data.links];
+      });
+    }
+  }
+
   return (
     <form onSubmit={saveProduct}>
       <label>Product name</label>
       <input type="text" placeholder="product name" value={title} onChange={(ev) => setTitle(ev.target.value)} />
+      <label>Photos</label>
+      <div className="mb-2 flex flex-wrap gap-2">
+        {!!images?.length &&
+          images.map((link) => (
+            <div key={link} className="h-24">
+              <img src={link} alt="" className="rounded-lg" />
+            </div>
+          ))}
+        <label className="w-24 h-24 cursor-pointer flex flex-col items-center justify-center text-gray-500 rounded-lg bg-gray-200">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth={1.5}
+            stroke="currentColor"
+            className="w-6 h-6"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M3 16.5v2.25A2.25 2.25 0 005.25 21h13.5A2.25 2.25 0 0021 18.75V16.5m-13.5-9L12 3m0 0l4.5 4.5M12 3v13.5"
+            />
+          </svg>
+          Upload
+          <input type="file" onChange={uploadImage} className="hidden" />
+        </label>
+        {/* {!images?.length && <div>No photos in this product</div>} */}
+      </div>
       <label>Description</label>
       <textarea placeholder="description" value={description} onChange={(ev) => setDescription(ev.target.value)} />
       <label>Price (in USD)</label>
